24b6d8c7db439259916edbea0acf1d9fcbeb5988
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2023-08-03 16:15:22 +0100

    hello word

--- myApp.js
+++ myApp.js
@@ -1,39 +1,9 @@
-let express = require('express');
-let app = express();
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  console.log("Hello World");
+  res.send("Hello World");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
