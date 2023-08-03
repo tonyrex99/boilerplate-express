1f9ce26eb7596b0455e2e9a715def3b98972fd7d
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2023-08-03 20:06:29 +0100

    done

--- myApp.js
+++ myApp.js
@@ -1,9 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
-  console.log("Hello World");
-  res.send("Hello World");
+  console.log("Hello Expressed");
+  var absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
+app.get("/json", (req, res) => {
+  const message =
+    process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON" : "Hello json";
+  console.log(message);
+  res.json({ message: message });
 });
 
 module.exports = app;
